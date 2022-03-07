98565238b411839124d6f22a9196742850b2a127
Huiling Chen <hlingchen95@gmail.com>
2022-03-07 03:49:31 -0500

    add first DB migration

--- MovieShopMVC/MovieShopMVC/Program.cs
+++ MovieShopMVC/MovieShopMVC/Program.cs
@@ -1,11 +1,18 @@
 using ApplicationCore.Contracts.Services;
+using Infrastructure.Data;
 using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IMovieService, MovieService>();
+
+builder.Services.AddDbContext<MovieShopDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieShopDbConnection"));
+});
 
 var app = builder.Build();
 
