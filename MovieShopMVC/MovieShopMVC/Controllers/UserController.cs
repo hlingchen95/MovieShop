cbb47d6534655dd503d7989b2e8555585365044e
Huiling Chen <hlingchen95@gmail.com>
2022-03-14 22:05:13 -0400

    used current user

--- MovieShopMVC/MovieShopMVC/Controllers/UserController.cs
+++ MovieShopMVC/MovieShopMVC/Controllers/UserController.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Services;
 using System.Security.Claims;
 
 namespace MovieShopMVC.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ICurrentUser _currentUser;
+
+        public UserController(ICurrentUser currentUser)
+        {
+            _currentUser = currentUser;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Purchases()
         {
-            
+            var userAuth = _currentUser.IsAuthenticated;
+            if (userAuth)
+            {
+                var userId = _currentUser.UserId;
+            }
             return View();
         }
 
